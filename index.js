93e3d9bdd17c7435623eba1a25e4bcadc6aa1295
rishabh mathur <rishabhmathur@gmail.com>
2020-11-11 12:07:23 +0530

    Hotel Manaagement

---
+++ index.js
@@ -0,0 +1,165 @@
+
+function fn1()
+{
+    $(document).ready(function(){
+       $('#date').datepicker({
+        numberOfMonths:1,
+        changeYear:true,
+        changeMonth:true,
+        showWeek:true,
+        weekHeader:"weak no",
+        showOtherMonths:true
+       });
+    });
+}
+function fn2()
+{
+    $(document).ready(function(){
+       $('#date2').datepicker({
+        numberOfMonths:1,
+        changeYear:true,
+        changeMonth:true,
+        showWeek:true,
+        weekHeader:"weak no",
+        showOtherMonths:true
+       });
+    });
+}
+let temp=false;
+function display()
+{   if(temp==false)
+	{
+	document.getElementById('test').style.visibility="visible";
+	temp=true;
+	}
+	// else
+	// {
+	// document.getElementById('test').style.visibility="hidden";
+	// temp=false;
+	// }
+}
+function displaynext()
+{
+  if(temp=true)
+  {
+    	document.getElementById('test').style.visibility="hidden";
+	temp=false;
+  }
+}
+function display2()
+{   if(temp==false)
+	{
+	document.getElementById('test2').style.visibility="visible";
+	temp=true;
+	}
+	else
+	{
+	document.getElementById('test2').style.visibility="hidden";
+	temp=false;
+	}
+}
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+var slideIndex1 = 1;
+showSlides1(slideIndex1);
+
+function plusSlides1(n) {
+  showSlides1(slideIndex1 += n);
+}
+
+function currentSlide1(n) {
+  showSlides1(slideIndex1 = n);
+}
+
+function showSlides1(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides1");
+  var dots = document.getElementsByClassName("dot1");
+  if (n > slides.length) {slideIndex1 = 1}    
+  if (n < 1) {slideIndex1 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex1-1].style.display = "block";  
+  dots[slideIndex1-1].className += " active";
+}
+
+var slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+function plusSlides2(n) {
+  showSlides2(slideIndex2 += n);
+}
+
+function currentSlide2(n) {
+  showSlides2(slideIndex2 = n);
+}
+
+function showSlides2(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides2");
+  var dots = document.getElementsByClassName("dot2");
+  if (n > slides.length) {slideIndex2 = 1}    
+  if (n < 1) {slideIndex2 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex2-1].style.display = "block";  
+  dots[slideIndex2-1].className += " active";
+}
+var slideIndex3 = 1;
+showSlides3(slideIndex3);
+
+function plusSlides3(n) {
+  showSlides3(slideIndex3 += n);
+}
+
+function currentSlide3(n) {
+  showSlides3(slideIndex3 = n);
+}
+
+function showSlides3(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides3");
+  var dots = document.getElementsByClassName("dot3");
+  if (n > slides.length) {slideIndex3 = 1}    
+  if (n < 1) {slideIndex3 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex3-1].style.display = "block";  
+  dots[slideIndex3-1].className += " active";
+}
---
+++ index2.js
@@ -0,0 +1,45 @@
+var flag=false;
+function destination(){
+    if(flag==false)
+	{
+	document.getElementById('dest1').style.visibility="visible";
+	flag=true;
+    }}
+    function destination2()
+	{
+        if(flag=true)
+        {
+	document.getElementById('dest1').style.visibility="hidden";
+	flag=false;
+	}
+}
+var flag=false;
+function experiences(){
+    if(flag==false)
+	{
+	document.getElementById('exp').style.visibility="visible";
+	flag=true;
+    }}
+    function experiences2()
+	{
+        if(flag=true)
+        {
+	document.getElementById('exp').style.visibility="hidden";
+	flag=false;
+	}
+}
+var flag=false;
+function offer(){
+    if(flag==false)
+	{
+	document.getElementById('special').style.visibility="visible";
+	flag=true;
+    }}
+    function offer2()
+	{
+        if(flag=true)
+        {
+	document.getElementById('special').style.visibility="hidden";
+	flag=false;
+	}
+}
